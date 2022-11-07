1b736bc5d4962e5924abfe73f7e8608b840334ff
Gary Dineen <dineen77@hotmail.com>
2022-11-07 18:01:01 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
-    export const App = () => {
-    return (
-        <div>App</div>
-    )
-    }
+import "./App.css";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
-    export default App
+
+const App = () => {
+	return (
+		<Box width="400px" sx={{ width: { xl: "1488px" } }}>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/exercise/:id" element={<ExerciseDetail />} />
+			</Routes>
+			<footer />
+		</Box>
+	);
+};
+
+export default App;
+
+/* notes*/
+/* Box is div with colours  from material ui */
+/* sx= size and width on big and small screen*/
+/* m= which mean marign is auto*/
